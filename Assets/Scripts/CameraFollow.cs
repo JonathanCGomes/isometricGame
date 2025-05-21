85d003718d2f652c2f5b83b6ab6cb39db34183bb
Jonathan Cesar <jonathan10ccg@gmail.com>
2025-05-21 13:01:09 -0300

    OffSet Camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform playerTransform; // Referencia ao jogador
+    [SerializeField]
     private Vector3 offset;           // Diferenca entre a posicao da camera e do jogador
 
     private void Start()

c822bb8498d65e2480d69a614656d11e6af8a61a
Jonathan Cesar <jonathan10ccg@gmail.com>
2025-05-21 09:38:54 -0300

    Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform; // Referencia ao jogador
+    private Vector3 offset;           // Diferenca entre a posicao da camera e do jogador
+
+    private void Start()
+    {
+        offset = transform.position - playerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
